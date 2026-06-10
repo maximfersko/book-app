25f37be118a72305591bd3a5ecfa659f5fe100ba
maxim <bobrovmaksim35@gmail.com>
2026-06-10 16:11:46 +0700

    optimize imports

--- book/src/main/java/com/web/book/model/Borrowing.java
+++ book/src/main/java/com/web/book/model/Borrowing.java
@@ -1,8 +1,6 @@
 package com.web.book.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
--- book/src/main/java/com/web/book/service/impl/BookServiceImpl.java
+++ book/src/main/java/com/web/book/service/impl/BookServiceImpl.java
@@ -9,11 +9,10 @@ import com.web.book.repository.BookRepository;
 import com.web.book.repository.BorrowingRepository;
 import com.web.book.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
